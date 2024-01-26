ebef6fd257457404fd226642241fb5224febd7dd
sakshi1feb <sakshi1feb@gmail.com>
2024-01-26 12:51:37 +0530

    change the name of web app

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Nest Quest</h1>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Home Heaven</h1>
     <p className='mb-4 text-slate-700'>
-      Nest Quest is a real estate marketplace where you can find your next
+      Home Heaven is a real estate marketplace where you can find your next
       perfect place to live. We provide the best service to our customers
       and we are always looking to improve our service.
     </p>
     <p className='mb-4 text-slate-700'>
       Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
-      </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
-    </div>
+    </p>
+    <p className='mb-4 text-slate-700'>
+      Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.
+    </p>
+  </div>
   )
 }
